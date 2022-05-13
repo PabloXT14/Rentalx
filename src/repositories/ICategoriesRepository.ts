cdd61c5aeff366035473f4988e704b3097f00f5f
Pablo Alan de Freitas Cardoso <pabloxt14@gmail.com>
2022-05-12 21:24:39 -0300

    feat: review code again

--- src/repositories/ICategoriesRepository.ts
+++ src/repositories/ICategoriesRepository.ts
@@ -1,5 +1,6 @@
 import { Category } from "../models/Category";
 
+// DTO (Data transfer object) - conceito de criar um objeto/tipagem que será responsável pela trasferência de dados de uma camada/classe para outra da aplicação.
 interface ICreateCategoryDTO {
     name: string;
     description: string;
