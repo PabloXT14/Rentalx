4433b53bca1543e467daaeaddd6e45f4ec7a6469
Pablo Alan de Freitas Cardoso <pabloxt14@gmail.com>
2022-05-17 20:09:42 -0300

    feat: continuing make project

--- src/modules/cars/repositories/ISpecificationsRepository.ts
+++ src/modules/cars/repositories/ISpecificationsRepository.ts
@@ -1,10 +1,13 @@
+import { Specification } from "../models/Specification";
+
 interface ICreateSpecificationDTO {
     name: string;
     description: string;
 }
 
 interface ISpecificatonsRepository {
-    create() {
+    create({ name, description }: ICreateSpecificationDTO): void;
+    findByName(name: string): Specification | undefined;
+}
 
-}
-}
+export { ISpecificatonsRepository, ICreateSpecificationDTO }
--- src/modules/cars/services/CreateSpecificationService.ts
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -1,8 +1,25 @@
+import { SpecificationRepository } from "../repositories/SpecificationsRepository";
+
+interface IRequest {
+    name: string;
+    description: string;
+}
 
 
 class CreateSpecificationService {
-    execute() {
-        console.log("TODO");
+    constructor(private specificationsRepository: SpecificationRepository) { }
+
+    execute({ name, description }: IRequest): void | Error {
+        const specificationAlreadyExists = this.specificationsRepository.findByName(name);
+
+        if (specificationAlreadyExists) {
+            throw new Error("Specification already exists!");
+        }
+
+        this.specificationsRepository.create({
+            name,
+            description
+        })
     }
 }
 
