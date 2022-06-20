a474d566db0d16decd93d1780d402401d863325d
Pablo Alan <pabloxt14@gmail.com>
2022-06-20 17:51:32 -0300

    feat: start uploading of files

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,9 +1,14 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
 import { listCategoriesController } from '../modules/cars/useCases/listCategories';
 
 const categoriesRoutes = Router();
+
+const upload = multer({
+    dest: './tmp',
+});
 
 // Rota Cadastro de Categories
 categoriesRoutes.post('/', (request, response) => {
@@ -13,6 +18,15 @@ categoriesRoutes.post('/', (request, response) => {
 // Rota Listar todas as Categories
 categoriesRoutes.get('/', (request, response) => {
     return listCategoriesController.handle(request, response);
+});
+
+// Rota de Upload de arquivos
+categoriesRoutes.post('/import', upload.single('file'), (request, response) => {
+    const { file } = request.body;
+
+    console.log(file);
+
+    return response.status(200).json('File Uploaded!');
 });
 
 export { categoriesRoutes };
