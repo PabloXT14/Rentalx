2a98435ccdb5700e86d098e1507a344f329120cd
Pablo Alan <pabloxt14@gmail.com>
2022-06-27 18:15:46 -0300

    feat: stream concept

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,6 +1,21 @@
+import { parse } from 'csv-parse';
+import fs from 'fs';
+
 class ImportCategoryUseCase {
-    execute(file: any) {
-        console.log(file);
+    execute(file: Express.Multer.File): void {
+        // CRIANDO STREAM DE LEITURA DO ARQUIVO RECEBIDO PELA ROTA
+        const stream = fs.createReadStream(file.path); // passando caminho do arquivo
+
+        // BIBLIOTECA CSVPARSE PARA CONVERTER NOSSO ARQUIVO PARA UM FORMATO ESPECIFICO
+        const parseFile = parse();
+
+        // PIPE: SERVE PARA PASSAR O PEDAÇO LIDO DO STREAM PARA ALGUM LUGAR/AÇÃO
+        stream.pipe(parseFile);
+
+        // LENDO O QUE FOI RECEBIDO
+        parseFile.on('data', async line => {
+            console.log(line);
+        });
     }
 }
 
